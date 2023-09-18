0263280ae05a0ff6b4dd167e858fc0d293fbbf92
lleonardofarias <Mantzzcanal@gmail.com>
2023-09-18 13:57:56 -0300

    Add Weahter icon and Current Weather

--- passar_clima/views.py
+++ passar_clima/views.py
@@ -3,16 +3,20 @@ from django.http import JsonResponse
 import requests
 from datetime import datetime
 
+def get_weather_icon(icon_code):   #use the openweather to get weather icons
+    icon_url = f"http://openweathermap.org/img/w/{icon_code}.png"
+    return icon_url
+
 def weather_api(request):
     city = request.GET.get('city', '')
-    unit = request.GET.get('unit')  
+    unit = request.GET.get('unit')
     api_key = "your api key"
     base_url = "your weather api url"
 
     params = {
         "q": city,
         "appid": api_key,
-        "units": unit  
+        "units": unit
     }
 
     response = requests.get(base_url, params=params)
@@ -21,11 +25,17 @@ def weather_api(request):
         weather_info = response.json()
         temperature = weather_info['main']['temp']
         timestamp = datetime.now(pytz.timezone(request.GET.get('timezone', 'UTC'))).strftime('%H:%M:%S')
+        current_weather = weather_info['weather'][0]['description']  
+        icon_code = weather_info['weather'][0]['icon']  
+        icon_url = get_weather_icon(icon_code)  
+        
         response_data = {
             "temperature": temperature,
             "city": city,
-            "timestamp": timestamp
+            "timestamp": timestamp,
+            "current_weather": current_weather,
+            "icon_url": icon_url  
         }
         return JsonResponse(response_data)
     else:
-        return JsonResponse({"error": "Fail in obtain data"}, status=500)
+        return JsonResponse({"error": "Fail to obtain data"}, status=500)
